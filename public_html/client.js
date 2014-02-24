f41d07909a0870d490f21c377eb0bd36192aec09
Hugo <hugo.y.charles@gmail.com>
2014-02-24 13:32:53 +0100

    tweet gauche droite

--- public_html/client.js
+++ public_html/client.js
@@ -8,23 +8,33 @@
 (function($) {
 
     var socket = io.connect('http://localhost:8080');
+    var gaucheDroite = 0;
 
     // Envoie du hastage au serveur
     $('#hastagform').submit(function(event) {
         event.preventDefault();
-        console.log($('#hashtag').val());
+        //console.log($('#hashtag').val());
         socket.emit('search', {
             hashtag: $('#hashtag').val()
         });
     })
-    
+
     // Nouveau tweet
     socket.on('newtweet', function(message) {
-        console.log(message);
+        //console.log(message);
+        console.log(typeof message.date);
         var msgtpl = $('#mustache-modele-tweet').html();
-
-        $('#les-tweets').append(' <div class="row tweet">' + Mustache.render(msgtpl, message) + '</div>');
-
+        var gauche = '<div class="col-sm-8 col-sm-offset-2">';
+        var droite = '<div class="col-sm-8 col-sm-offset-4">';
+        var cote;
+        if(gaucheDroite % 2 === 0){
+            cote = gauche;
+        }else{
+            cote = droite;
+        }
+        gaucheDroite++;
+        $('#les-tweets').prepend('<div class="row tweet">'+cote + Mustache.render(msgtpl, message) + '</div></div>');
+        
     });
-    
+
 })(jQuery); 
